e600fbaca0b68d50625cd71146e49b6aa1b92d64
VeneCross <santiago.vasquez2207@gmail.com>
2026-06-22 22:56:10 -0300

    Update NodoListaProducto.java

--- tda/NodoListaProducto.java
+++ tda/NodoListaProducto.java
@@ -3,11 +3,17 @@ package tda;
 import model.Producto;
 
 public class NodoListaProducto {
+    // Acá se guarda el producto que contiene este nodo
     Producto dato;
+
+    // Referencia al siguiente nodo de la lista
     NodoListaProducto siguiente;
 
     public NodoListaProducto(Producto dato) {
+        // Guardo el producto que me pasan al crear el nodo
         this.dato = dato;
+
+        // Arranca sin siguiente porque todavía no está enlazado con otro nodo
         this.siguiente = null;
     }
 }
