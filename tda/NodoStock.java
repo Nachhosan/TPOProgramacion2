82ad42c0c7ceda18361c5e41baf71b48ffddf85a
VeneCross <santiago.vasquez2207@gmail.com>
2026-06-22 22:54:13 -0300

    Update NodoStock.java

--- tda/NodoStock.java
+++ tda/NodoStock.java
@@ -3,13 +3,23 @@ package tda;
 import model.Producto;
 
 public class NodoStock {
+    // Acá se guarda el producto que va en este nodo del árbol
     Producto dato;
+
+    // Referencia al hijo izquierdo
     NodoStock izq;
+
+    // Referencia al hijo derecho
     NodoStock der;
 
     public NodoStock(Producto dato) {
+        // Cuando creo el nodo, le asigno el producto que va a guardar
         this.dato = dato;
+
+        // Arranca sin hijo izquierdo
         this.izq = null;
+
+        // Arranca sin hijo derecho
         this.der = null;
     }
 }
